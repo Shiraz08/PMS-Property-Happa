07450fae581675b780826a24f2024a8e22b94557
Shiraz Naseer <65015611+Shiraz08@users.noreply.github.com>
2024-02-18 22:40:08 +0500

    update

---
+++ PMS-PropertyHapa/Controllers/ApplicationsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMS_PropertyHapa.Controllers
+{
+    public class ApplicationsController : Controller
+    {
+        public IActionResult AddApplication()
+        {
+            return View();
+        }
+        public IActionResult ViewApplication()
+        {
+            return View();
+        }
+    }
+}
---
+++ PMS-PropertyHapa/Controllers/SupportCenterController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PMS_PropertyHapa.Controllers
+{
+    public class SupportCenterController : Controller
+    {
+        public IActionResult AddTickets()
+        {
+            return View();
+        }
+        public IActionResult ViewTickets()
+        {
+            return View();
+        }
+    }
+}
