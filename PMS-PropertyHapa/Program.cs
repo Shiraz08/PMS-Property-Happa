93d4403caf0af765156170a2d7e4774eb9af7c2b
Shiraz Naseer <65015611+Shiraz08@users.noreply.github.com>
2024-02-25 13:29:21 +0500

    update code

--- PMS-PropertyHapa/Program.cs
+++ PMS-PropertyHapa/Program.cs
@@ -80,6 +80,6 @@ app.UseAuthorization();
 app.UseSession();
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Auth}/{action=Login}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
