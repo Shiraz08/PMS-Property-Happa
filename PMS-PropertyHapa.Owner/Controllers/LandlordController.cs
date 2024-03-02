cd1c57b58f239fbf8b969ef968f4fbcc58544173
samtahir2451998 <78282179+samtahir2451998@users.noreply.github.com>
2024-03-02 17:54:18 +0500

    updated

---
+++ PMS-PropertyHapa.Owner/Controllers/LandlordController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using PMS_PropertyHapa.MigrationsFiles.Data;
+using PMS_PropertyHapa.Models;
+using PMS_PropertyHapa.Models.DTO;
+using PMS_PropertyHapa.Models.Entities;
+using PMS_PropertyHapa.Models.Roles;
+using PMS_PropertyHapa.Owner.Controllers;
+using PMS_PropertyHapa.Owner.Services.IServices;
+using PMS_PropertyHapa.Shared.Enum;
+using PMS_PropertyHapa.Shared.ImageUpload;
+
+namespace PMS_PropertyHapa.Staff.Controllers
+{
+    public class LandlordController : Controller
+    {
+        private readonly IAuthService _authService;
+        private readonly ITokenProvider _tokenProvider;
+        private readonly ILogger<HomeController> _logger;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private ApiDbContext _context;
+        private readonly IUserStore<ApplicationUser> _userStore;
+        private IWebHostEnvironment _environment;
+        public LandlordController(IAuthService authService, ITokenProvider tokenProvider, IWebHostEnvironment Environment, ILogger<HomeController> logger, SignInManager<ApplicationUser> signInManager, UserManager<ApplicationUser> userManager, ApiDbContext context, IUserStore<ApplicationUser> userStore)
+        {
+            _authService = authService;
+            _tokenProvider = tokenProvider;
+            _logger = logger;
+            _signInManager = signInManager;
+            _userManager = userManager;
+            _context = context;
+            _userStore = userStore;
+            _environment = Environment;
+        }
+        public IActionResult Index()
+        {
+            var list = _context.Owner.AsNoTracking().ToList();
+            return View(list);
+        } 
+        public IActionResult AddLandlord()
+        {
+            return View();
+        }
+    }
+}
